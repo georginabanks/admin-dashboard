a875aeb7880d33bd118f43ae4111033fa9dc0659
Georgina Banks <georginabanks01@gmail.com>
2023-09-13 20:45:11 +0100

    initial commit

---
+++ src/client/api.jsx
@@ -0,0 +1,85 @@
+export const posts = [
+	{
+		postTitle: "post 1",
+		postContent: "post 1 content",
+		postStatus: "1",
+		status: "published",
+		datePublished: "2023-09-13T00:00:00Z",
+		postSlug: "post-1",
+		postUser: "1",
+		username: "georginabanks"
+	},
+	{
+		postTitle: "post 2",
+		postContent: "post 2 content",
+		postStatus: "1",
+		status: "published",
+		datePublished: "2023-09-13T00:00:00Z",
+		postSlug: "post-2",
+		postUser: "1",
+		username: "georginabanks"
+	},
+	{
+		postTitle: "post 3",
+		postContent: "post 3 content",
+		postStatus: "1",
+		status: "published",
+		datePublished: "2024-09-13T00:00:00Z",
+		postSlug: "post-3",
+		postUser: "1",
+		username: "georginabanks"
+	},
+	{
+		postTitle: "post 4",
+		postContent: "post 4 content",
+		postStatus: "2",
+		status: "draft",
+		datePublished: "2024-09-13T00:00:00Z",
+		postSlug: "post-4",
+		postUser: "1",
+		username: "georginabanks"
+	}
+]
+
+export const pages = [
+	{
+		pageTitle: "page 1",
+		pageContent: "page 1 content",
+		pageStatus: "1",
+		status: "published",
+		datePublished: "2023-09-13T00:00:00Z",
+		pageSlug: "page-1",
+		pageUser: "1",
+		username: "georginabanks"
+	},
+	{
+		pageTitle: "page 2",
+		pageContent: "page 2 content",
+		pageStatus: "1",
+		status: "published",
+		datePublished: "2023-09-13T00:00:00Z",
+		pageSlug: "page-2",
+		pageUser: "1",
+		username: "georginabanks"
+	},
+	{
+		pageTitle: "page 3",
+		pageContent: "page 3 content",
+		pageStatus: "1",
+		status: "published",
+		datePublished: "2024-09-13T00:00:00Z",
+		pageSlug: "page-3",
+		pageUser: "1",
+		username: "georginabanks"
+	},
+	{
+		pageTitle: "page 4",
+		pageContent: "page 4 content",
+		pageStatus: "2",
+		status: "draft",
+		datePublished: "2024-09-13T00:00:00Z",
+		pageSlug: "page-4",
+		pageUser: "1",
+		username: "georginabanks"
+	}
+]
