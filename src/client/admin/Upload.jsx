7a19add69201fbb2bb32e1ca5fcec7bfed3e3644
Georgina Banks <georginabanks01@gmail.com>
2023-09-15 13:04:17 +0100

    upload success text

--- src/client/admin/Upload.jsx
+++ src/client/admin/Upload.jsx
@@ -4,17 +4,29 @@ import { useState } from "react";
 export default function Upload() {
 	
 	const [image, setImage] = useState();
+	const [success, setSuccess] = useState("");
 	
 	const handleSubmit = ( event ) => {
 		event.preventDefault();
 		axios.post("/upload", image, { headers: { "Content-Type": "multipart/form-data" }})
-				.then(res => console.log(res))
+				.then(res => {
+					console.log(res);
+					if (res.status === 200) {
+						setSuccess("Successfully uploaded!")
+					} else {
+						setSuccess("Upload failed")
+					}
+				})
 	}
 	
 	return (
-			<form onSubmit={handleSubmit} encType={"multipart/form-data"}>
-				<input type="file" name="image" onChange={ e => setImage({ image: e.target.files[0] }) }/>
-				<button type="submit">Upload</button>
-			</form>
+			<div>
+				<form onSubmit={handleSubmit} encType={"multipart/form-data"}>
+					<input type="file" name="image" onChange={ e => setImage({ image: e.target.files[0] }) }/>
+					<button type="submit">Upload</button>
+				</form>
+				
+				<p>{success}</p>
+			</div>
 	)
 }
