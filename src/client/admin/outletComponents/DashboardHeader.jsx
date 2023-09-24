cfef7ee8bc0f90f7eb247dcabcf6821a429d8aa1
Georgina Banks <georginabanks01@gmail.com>
2023-09-24 21:00:40 +0100

    dashboard greeting

--- src/client/admin/outletComponents/DashboardHeader.jsx
+++ src/client/admin/outletComponents/DashboardHeader.jsx
@@ -1,11 +1,26 @@
 export default function DashboardHeader({ cookies }) {
-	
+	console.log(cookies)
 	let greeting = cookies.username;
 	if ( cookies.name !== null ) { greeting = cookies.name }
 	
 	return (
 			<div className={'outlet-header'}>
-				Hello, { greeting }!
+				<div className={'row'}>
+					<div className={'greeting'}>
+						<div className={'row'}>
+							<div className={'col-3'}>
+								<div className={'square'}>
+									<img src={ cookies.filename !== null ? '/uploads/' + cookies.filename : '/uploads/default.jpeg' }
+										 alt={ cookies.alt } className={'round-image'} />
+								</div>
+							</div>
+							
+							<div className={'col-auto'}>
+								<h6 className={'greeting-text'}>Hello, { greeting }!</h6>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 	)
 }
